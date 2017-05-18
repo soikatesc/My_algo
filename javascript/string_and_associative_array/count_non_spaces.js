23265f23ad1e00157bdf52654ac443c13f16643c
Md Rahaman <soikatesc@gmail.com>
2017-05-17 23:12:48 -0500

    modified count non spaces in a string

--- javascript/string_and_associative_array/count_non_spaces.js
+++ javascript/string_and_associative_array/count_non_spaces.js
@@ -1,1 +1,14 @@
+
+function countNonSpace(str){
+	str_split = str.split("")
+	var count = 0;
+	for(var i=0; i < str.length; i++){
+		if(str[i] != " "){
+			count++;
+		}
+	}
+	return count;
+}
+
 str = "Honey pie, you are driving me crazy"
+console.log(countNonSpace(str))2
