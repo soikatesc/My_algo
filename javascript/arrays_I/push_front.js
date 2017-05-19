88a475d4981bcae42bac409d8192001955f1a57a
Md Rahaman <soikatesc@gmail.com>
2017-05-18 23:37:07 -0500

    push a value fornt of the array built in method

---
+++ javascript/arrays_I/push_front.js
@@ -0,0 +1,14 @@
+function pushFront(arr, val){
+	//method1
+	arr.unshift(5)
+	console.log(arr)
+	// for(var i=0; i<arr.length-1; i++){
+	// 	temp = arr[i]
+	// 	arr[i+1] = temp
+	// }
+	// console.log(arr)
+}
+
+arr = [3,5,6]
+pushFront(arr, 5)
+
