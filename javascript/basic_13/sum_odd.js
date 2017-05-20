4b24f96612d11b2908a3174ace3aa5696289b3da
Md Rahaman <soikatesc@gmail.com>
2017-05-19 22:40:54 -0500

    sum of odd number between a range

---
+++ javascript/basic_13/sum_odd.js
@@ -0,0 +1,13 @@
+function sumOdd(start, end){
+	var sum = 0;
+	for(var i=start; i<=end; i++){
+		if(i%2 != 0){
+			sum = sum + i;
+		}
+	}
+	console.log(sum)
+}
+
+var start = 1;
+var end = 255;
+sumOdd(start, end)
