f8a872918b14ce810f471fbb7f49e462c6e62392
Md Rahaman <soikatesc@gmail.com>
2017-06-01 23:05:36 -0500

    recursion fibonacchi modified

--- javascript/recarsion/fibonnachi.js
+++ javascript/recarsion/fibonnachi.js
@@ -1,4 +1,4 @@
-var readline = require('readline')
+
 
 function fib_series(small, large, n){
 	var sum;
@@ -11,5 +11,27 @@ function fib_series(small, large, n){
 	fib_series(large,sum,n-1)
 }
 
-var n=3
-console.log(fib_series(-1,1,n))
+// var n=3
+// console.log(fib_series(-1,1,n))
+
+function rFib(num){
+	sum = 0
+	if(num <= 0 || num == 1){
+		return num
+	}else{
+		console.log(num)
+		return rFib(num - 1) + rFib(num - 2)
+	}
+}
+
+//fibonacchi series fast
+function fib_term_fast(small, large, n){
+	if(n == -1){
+		return large
+	}
+	sum = small + large
+	console.log('sum -> ', sum)
+	return fib_term_fast(large, sum, n-1)
+}
+console.log(fib_term_fast(-1, 1, 10))
+
