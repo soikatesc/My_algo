146923ad80c1fe873be5cf8c5dcc34c37e2ea4bf
Md Rahaman <soikatesc@gmail.com>
2017-06-24 01:27:17 -0500

    added binary search tree 2 functions: add, contains

---
+++ javascript/binary_trees/binary_tree.js
@@ -0,0 +1,70 @@
+
+//node 
+function BTNode(value){
+	this.value = value;
+	this.left = null;
+	this.right = null;
+}
+
+//constructior for binary search tree
+function BST(){
+	this.root = null;
+}
+
+BST.prototype.add = function(value){
+	var root = this.root;
+
+	if(!root){
+		this.root = new BTNode(value)
+		return this;
+	}
+
+	var runner = this.root
+	var newNode = new BTNode(value)
+
+	while(runner){
+		if(value < runner.value){
+			if(!runner.left){
+				runner.left = newNode
+				return this;
+			}
+			else{
+				runner = runner.left;
+			}
+		}
+		else{
+			if(!runner.right){
+				runner.right = newNode;
+				return this;
+			}
+			else{
+				runner = runner.right
+			}
+		}
+	}
+}
+
+BST.prototype.contains = function(value){
+	var runner = this.root
+
+	while(runner){
+		if(value == runner.value){
+			return true;
+		}
+		if(value < runner.value){
+			runner = runner.left;
+		}else{
+			runner = runner.right;
+		}
+	}
+	return false;
+}
+
+var bst = new BST()
+bst.add(5)
+bst.add(6)
+bst.add(7)
+bst.add(4)
+console.log(bst.contains(10))
+
+
