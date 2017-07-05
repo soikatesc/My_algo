b40a3f9e6f812859a1894c003a421ad44d6662bd
Md Rahaman <soikatesc@gmail.com>
2017-07-04 22:36:26 -0500

    added ES6 folder

---
+++ javascript/es6/es6.js
@@ -0,0 +1,15 @@
+//let insted of var
+//var scoping issue, wasn't clear where to use
+
+//deaclaring a variable using let; the variable is accessible is same block
+
+//decalar let move ; saving a memory
+//initialize le moie="the notebook" ; giving a value
+
+function theNotebook(){
+	let movie = 'The Noteboook';
+	return movie
+}
+
+console.log(theNotebook());
+// console.log(movie) it will give error
