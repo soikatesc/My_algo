05d4f932ca492c8dedf8f561ac1c07f9cc0e00be
Md Rahaman <soikatesc@gmail.com>
2017-07-04 23:20:46 -0500

    updated classes with inheritance

--- javascript/es6/classes.js
+++ javascript/es6/classes.js
@@ -5,10 +5,48 @@ class Person{
 		this.weight = weight;
 	}
 
+	displayName(){
+		console.log(this.name)
+	}
+
+	displayAge(){
+		console.log(this.age)
+	}
+
 	displayWeight(){
 		console.log(this.weight)
+	}
+
+	// let displayWeight = () => {
+	// 	console.log(this.weight)
+	// }
+}
+
+// Parant is the super class
+//programmer is the child class
+
+class Programmer extends Person {
+	constructor(name, age, weight, language){
+		super(name, age, weight)
+		this.language = language
+	}
+
+	displayLanguage(){
+		console.log(this.language)
 	}
 }
 
 let soikat = new Person('Soikat', 27, '130lb')
 soikat.displayWeight()
+soikat.displayName()
+soikat.displayAge()
+
+//onle programmer has access for displayLanguage function
+md = new Programmer('Md', 27, '130lb', 'javascript')
+md.displayWeight()
+md.displayName()
+md.displayAge()
+md.displayLanguage()
+
+
+
