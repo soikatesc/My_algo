de04df4d236a370ba9917cf025431a8344bb7e85
Md Rahaman <soikatesc@gmail.com>
2017-07-04 22:48:53 -0500

    template literals

---
+++ javascript/es6/templates_literals.js
@@ -0,0 +1,15 @@
+//old way
+let name = 'Bucky'
+
+console.log('My favorite person is' + name + 'because')
+//new way
+console.log(`My favorite person is ${name} because he smells good`)
+
+let a = 5;
+let b = 7;
+
+console.log(`My favorite person is ${a+b} because he smells good.`)
+
+//interpretering as a new line
+console.log(`My favorite person is ${name} because
+he smells good`)
