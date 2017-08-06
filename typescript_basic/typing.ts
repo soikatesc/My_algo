e8e8f5714d9ab2eb6f255788b29b97b2978a68ad
Md Rahaman <soikatesc@gmail.com>
2017-08-05 22:14:33 -0500

    typescript basic and object oriented

---
+++ typescript_basic/typing.ts
@@ -0,0 +1,13 @@
+let myString: string;
+// myString = 'This is a string';
+
+let aNumber: number;
+let aBoolean: boolean;
+let aString: string;
+let anArray: Array<string>;
+let anything: any;
+
+console.log(aNumber);
+
+// return type void, enums
+
