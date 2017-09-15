82b796b92fc31512acb6c1c601232ce5ee1f17d0
Md Rahaman <soikatesc@gmail.com>
2017-09-14 23:36:42 -0500

    typescript basic

---
+++ typescript_basic/classes.js
@@ -0,0 +1,23 @@
+var Car = (function () {
+    function Car(speed) {
+        this.speed = speed || 0;
+    }
+    Car.prototype.accelerate = function () {
+        this.speed++;
+    };
+    Car.prototype.throttle = function () {
+        this.speed--;
+    };
+    Car.prototype.getSpeed = function () {
+        console.log(this.speed);
+    };
+    Car.numberOfWheels = function () {
+        return 4;
+    };
+    return Car;
+}());
+// Instantiate (create) an object from a class
+var car = new Car(5);
+console.log(car);
+car.accelerate();
+car.getSpeed();
