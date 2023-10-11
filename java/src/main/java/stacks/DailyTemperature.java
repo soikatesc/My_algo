f0a53256ddf3e5b3115f679a256715a23ceabbee
mdrahaman <soikatesc@gmail.com>
2023-10-10 22:16:22 -0500

    daily temperature problem

---
+++ java/src/main/java/stacks/DailyTemperature.java
@@ -0,0 +1,27 @@
+package stacks;
+
+import java.util.Stack;
+
+public class DailyTemperature {
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int[] output = new int[temperatures.length];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int index = stack.pop();
+                output[index] = i - index;
+            }
+
+            stack.push(i);
+        }
+
+        return output;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = {73,74,75,71,69,72,76,73};
+        dailyTemperatures(temperatures);
+    }
+}
